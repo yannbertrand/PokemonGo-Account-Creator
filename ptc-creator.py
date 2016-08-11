d334ad54278fc60791eff714b7822fb9f9771833
Yann Bertrand <github@yann-bertrand.fr>
2016-08-11 23:21:12 +0200

    Add ptc-creator.py

---
+++ ptc-creator.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""ptc-creator.py - Create multiple PTC accounts using file."""
+
+from pikapy import *
+import pprint
+import time
+import threading
+import sys, getopt
+
+def create_ptc_account(username, password, email):
+  account_info = random_account(username, password, email)
+  print('Created new ptc account {}'.format(account_info[USERNAME]))
+
+with open(str(sys.argv[1])) as f:
+  credentials = [x.strip().split(':') for x in f.readlines()]
+
+for username, password, email in credentials:
+  create_ptc_account(username, password, email)
